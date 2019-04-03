15dba27ce248762c0acda259dd3f5025578f5cf3
songlairui <cnre@live.cn>
2019-04-03 22:46:18 +0800

    Update Demo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -10,16 +10,28 @@
       >vue-cli documentation</a>.
     </p>
     <div class="stage">
-      <h3>API Usage</h3>
-      <p>ResponseData:[ {{responseData}} ]</p>
-      <button @click="sendRequest">Click Button to request api data and set next line</button>
+      <h3>
+        Request List
+        <button @click="sendRequest">request '/whoami'</button>
+      </h3>
+      <ul class="request-trace">
+        <li
+          v-for="(item, idx) in requestList"
+          :key="idx"
+        >-{{item[0] && item[0].toLocaleTimeString()}} [GET][/whoami] {{ item[1] }}</li>
+      </ul>
     </div>
     <div class="stage">
-      <h3>Code Editor</h3>
-      <div ref="code" class="code" contenteditable="true" @input="editing"></div>
-      <button @click="loadCode">load raw code from Disk</button>
+      <h3>Edit '/whoami' controller</h3>
+      <div ref="code" class="code" contenteditable="true"></div>
+      <button @click="sendRequest">request '/whoami'</button>
+      <br>-then>
       <button @click="changeCode">eg: Change 'you' to 'U'</button>
+      -then->
       <button @click="saveCode">Save code</button>
+      <br>-then->
+      <button @click="sendRequest">request '/whoami'</button>
+      <br>You will see the change.
     </div>
     <h3>Essential Links</h3>
     <ul>
@@ -54,23 +66,25 @@ export default {
   },
   data() {
     return {
+      trace: 10,
+      requestList: [[, "Click Button Above to Request API"]],
       responseData: "to show response data"
     };
   },
   methods: {
     sendRequest() {
+      const { trace } = this;
       request.get(url, (err, response, body) => {
-        console.info(err, response, body);
-        this.responseData = body;
+        const data = err ? `[err]${err.message}` : body;
+        this.requestList = [[new Date(), data], ...this.requestList].slice(
+          0,
+          trace
+        );
       });
-    },
-    editing(e) {
-      // console.info(e.target.innerText);
     },
     loadCode(cb) {
       request.get(codeUrl, (err, res, body) => {
         this.$refs.code.innerText = body;
-        console.info(err, res, body);
         cb && cb(err, res, body);
       });
     },
@@ -94,7 +108,7 @@ export default {
     saveCode() {
       const { innerText } = this.$refs.code;
       request.post(codeUrl, { json: { raw: innerText } }, (err, res, body) => {
-        console.info(err, res, body);
+        // console.info(err, res, body);
       });
     }
   },
@@ -112,10 +126,6 @@ h3 {
 ul {
   list-style-type: none;
   padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
 }
 a {
   color: #42b983;
